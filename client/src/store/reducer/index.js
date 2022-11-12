405c2e6043ce745f9a91cc3e9db4d3539a0561d8
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-11-12 14:18:07 -0300

    console.logs eliminated

--- client/src/store/reducer/index.js
+++ client/src/store/reducer/index.js
@@ -18,7 +18,6 @@ export default function reducer(state = initialState, action){
              }
 
         case SEARCH_RECIPE:
-            console.log(action.payload)
             return {
                 ...state,
                 filteredRecipes: action.payload
