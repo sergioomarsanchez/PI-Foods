7a00c2db1e8802e8ed2c23cb9538cbce5d790a08
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-11-16 16:44:06 -0300

    homepage responsive added

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -19,7 +19,31 @@ export default function Home(){
     const indexOfLastRecipe = currentPage * recipesPerPage
     const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage
     const currentRecipes = filteredRecipes?.slice(indexOfFirstRecipe, indexOfLastRecipe)
-                                      
+    
+    const [isVisible, setIsVisible] = useState(0)
+
+    const toggleVisibility = ()=>{
+      if(window.scrollY > 300){
+        setIsVisible(100)
+      } else {
+        setIsVisible(0)
+      }
+    }
+  
+    const scrollToTop = ()=>{
+      window.scrollTo({
+        top:0,
+        behavior:"smooth"
+      })
+    }
+  
+    useEffect(() => {
+      window.addEventListener('scroll', toggleVisibility)
+      return () =>{
+        window.removeEventListener('scroll', toggleVisibility)
+      }
+    }, [])
+
     let dispatch = useDispatch()
 
     const paginado = function (pageNumber){
@@ -77,7 +101,9 @@ export default function Home(){
                     healthScore={recipe.healthScore}/>
                 }):<div className={style.loaderContainer}> <span className={style.loader}></span></div>
             }
+
                 </div>
+                <button id={style.toTop} onClick={scrollToTop} style={{opacity: isVisible}} className={style.noselect}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/></svg></button>
                 
                 
     </div>
